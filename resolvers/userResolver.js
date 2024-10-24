745235370882eb820df4b35cf0e4264a862a0788
mozzam123 <mozzam607@gmail.com>
2024-10-24 20:56:25 +0530

    added single user revolver

--- resolvers/userResolver.js
+++ resolvers/userResolver.js
@@ -1,10 +1,16 @@
-const UserModel  = require("./../models/userModel");
+const UserModel = require("./../models/userModel");
 
 const UserResolvers = {
   Query: {
     async users() {
       const users = await UserModel.find();
       return users;
+    },
+    async user(_, args) {
+      const user = await UserModel.find({_id: args.id});
+      console.log('id: ', args.id);
+      console.log(user);
+      return user;
     },
   },
 };
