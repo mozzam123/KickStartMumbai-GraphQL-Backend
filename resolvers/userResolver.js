1ff4f00e0cdda33da9036e5304dedf163064382a
mozzam123 <mozzam607@gmail.com>
2024-10-25 16:04:02 +0530

    added "addUser" mutation

--- resolvers/userResolver.js
+++ resolvers/userResolver.js
@@ -9,7 +9,33 @@ const UserResolvers = {
     async user(_, args) {
       // Use findById to get a single user
       const user = await UserModel.findById(args.id);
-      return user; 
+      return user;
+    },
+  },
+  Mutation: {
+    addUser: async (_, args) => {
+      try {
+        if (!args.user.name || !args.user.password) {
+          return Error("Please provide name and password fields");
+        }
+        const existingUser = await UserModel.findOne({ name: args.user.name });
+        if (existingUser) {
+          return Error("User already exists with this name");
+        }
+
+        // Create new user
+        const newUser = await UserModel.create({
+          name: args.user.name,
+          password: args.user.password,
+          email: args.user.email,
+        });
+        console.log(newUser);
+
+        return newUser;
+      } catch (error) {
+        console.error("Error in addUser:", error.message);
+        throw new Error(`Error in addUser: ${error.message}`);
+      }
     },
   },
 };
--- schema/userSchema.js
+++ schema/userSchema.js
@@ -9,6 +9,14 @@ type Query {
     users: [User]
     user(id:ID!): User
 }
+type Mutation {
+  addUser(user: addUserInput!): User
+}
+input addUserInput{
+  name: String!,
+  email: String,
+  password: String!
+}
 `;
 
 module.exports = { typeDefs };
